c4a1cad8a106bacf0ab2100e8f3d0e298745c67c
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-09 15:19:03 +0300

    yes

--- src/App.js
+++ src/App.js
@@ -13,24 +13,68 @@ const OtrdienasStundas = [
   "stunda ja",
   "yes"
 ];
-const visasStundas = {
-  Pirmdiena: [
-    "sports",
+
+const visasStundas = [
+  {
+    diena: "Pirmdiena",
+stundas: [
+  "sports",
     "dabaszinibas",
     "Socialas zinibas un vesture",
     "Krievu valoda"
   ]
-}
+},
+{
+  diena: "Otrdiena",
+stundas: [
+  "sgds",
+    "dabasdgdsgdsbas",
+    "Socialgsdgdsun vesture",
+    "Krigsdgsd valoda"
+  ]
+},
+{
+  diena: "Tresdiena",
+stundas: [
+  "sgdsgsdrts",
+    "dabasgsdgdsnibas",
+    "gdsgsdbas un vesture",
+    "Krievu valoda"
+  ]
+},
+{
+  diena: "Ceturdiena",
+stundas: [
+  "spgsdgsdgts",
+    "dabgdsgdsnibas",
+    "Socidsgdsgsds un vesture",
+    "gsdgdsgsdgsd valoda"
+  ]
+},
+
+{
+  diena: "Piektdiena",
+stundas: [
+  "shdshdsgsdts", 
+    "dabrrefbfbhrdfbhdbas",
+    "Ssdhrfdgbdgeesdvesture",
+    "Krievu valoda"
+  ]
+},
+]
+const DienasJSX = visasStundas.map((diena, indekss) => {
+  return <Diena key={indekss} diena = {diena.diena} stundas={diena.stundas}/>
+})
   return (  
     <>
+    
   <div> stundu saraksts jau tagad</div>
   <Diena diena="Pirmdiena" stundas={PirmdienasStundas}/>
   <Diena diena="Otrdiena" stundas={OtrdienasStundas}/>
+    {DienasJSX}
   </>
   )
 }
-
-
 export default App;
 
 //.forEach() ir masīva metode, kura apskata ikkatru masīva elementu, bet neko neatgriež(nav return)
